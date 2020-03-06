24d74d10b1d4fd31119a6f85cd95f6010ea44d11
Guanzhou Zhao <benzhao2866@gmail.com>
2020-03-07 08:13:15 +1300

    consume multiple context with displayName

--- src/context/AppUseContext.js
+++ src/context/AppUseContext.js
@@ -1,13 +1,18 @@
 import React from 'react';
 
 const ThemeContext = React.createContext("dard");
+ThemeContext.displayName = "ThemeContext";
+const LoggedInUserContext = React.createContext({name: "guest", tag: 0});
+LoggedInUserContext.displayName = "LoggedInUserContext"
 
 export default class AppUseContext extends React.Component {
   render() {
     return (
       <ThemeContext.Provider value="light">
-        <h2>App component</h2>
-        <Toolbar />
+        <LoggedInUserContext.Provider value={{name:"Ben", tag: 1}}>
+          <h2>App component</h2>
+          <Toolbar />
+        </LoggedInUserContext.Provider>
       </ThemeContext.Provider>
     )
   }
@@ -16,7 +21,11 @@ export default class AppUseContext extends React.Component {
 function Toolbar(props) {
   return (
     <div>
-      <h3>....Toolbar component </h3>
+      <LoggedInUserContext.Consumer>
+        {
+          (loggedInUser) => <h3>....Toolbar component Hi, {loggedInUser.name}</h3>
+        }
+      </LoggedInUserContext.Consumer>
       <ThemeButton />
     </div>
   )
@@ -27,9 +36,12 @@ class ThemeButton extends React.Component {
     return (
       <div>
         <h4>........ThemeButton component</h4>
-        <button>Button inside ThemeButton component with theme {this.context}</button>
+        <ThemeContext.Consumer>
+          {
+            (themeValue) => <button>Button inside ThemeButton component with theme {themeValue}</button>
+          }
+        </ThemeContext.Consumer>
       </div>
     )
   }
 }
-ThemeButton.contextType = ThemeContext;
